f514230bf9a5f91bfa7322ad5e1827939f0882ce
Yurii <yura.gvozd.2002@gmail.com>
2026-09-23 15:40:49 +0300

    'Solution'

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.14-slim
+ENV PYTHONUNBUFFERED=1
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY app ./app
+
+CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
+import os
+
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = os.getenv("API_KEY")
+
+    url = "https://api.weatherapi.com/v1/current.json"
+
+    params = {
+        "key": api_key,
+        "q": "Paris",
+    }
+
+    response = requests.get(url, params=params)
+
+    data = response.json()
+
+    print(data)
 
 
 if __name__ == "__main__":
